18677c39a41e14963a0b10cca94d1799e6e1cc51
Sean Brage <sean@telegraph.io>
2022-05-21 00:09:06 -0500

    feat: Create repro example

---
+++ src/components/HelloOtherWorld.vue
@@ -0,0 +1,5 @@
+<script lang="ts">
+// Delete this line, type import { defineComp and select defineComponent from candidates to repro.
+import { defineComponent, defineComponent } from "vue";
+
+</script>
